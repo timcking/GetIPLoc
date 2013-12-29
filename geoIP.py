330a843786d76ff12f2f73598ccc061dc9a7c253
Tim King <timcking@gmail.com>
2013-12-28 22:09:20 -0800

    Added wxGlade files

--- geoIP.py
+++ geoIP.py
@@ -17,10 +17,10 @@ def printRecord(tgt, date):
     country = rec['country_name']
     lon = rec['longitude']
     lat = rec['latitude']
+    print 'IP Addr:  ' + tgt
     print 'Date:     ' + date
-    print 'Target:   ' + tgt
     print 'Location: '+ str(city)+', '+ str(region)+', '+ str(country)
-    print 'Latitude: '+ str(lat)+ ', Longitude: '+ str(lon)
+    print 'Lat/Lon:  ' + str(lat) + ','+ str(lon)
     print ''
 
 def main():
@@ -30,8 +30,8 @@ def main():
             printRecord(row[0], row[1])
             
     # Test open Google maps
-    url = 'http://maps.google.com/?q=38.645,-121.4404'
-    webbrowser.open_new(url)
+    #url = 'http://maps.google.com/?q=38.645,-121.4404'
+    #webbrowser.open_new(url)
             
 if __name__ == '__main__':
     main()
