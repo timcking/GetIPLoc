8519ecf1ad9fdc4c6f8aaf7496e2f3ca0aab39b6
Tim King <timcking@gmail.com>
2013-12-31 20:22:34 -0800

    Map is done

--- geoIP.py
+++ geoIP.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 # TODO:
-#    Add UI
-#    Add 'Show in Google Maps'
-#      maps query: http://maps.google.com/?q=38.645,-121.4404
+#    Get (scrape?) loc data directly from web page
+#    Move code out of list OnClick event
 
 import wx
 from wx import xrc
@@ -12,6 +11,7 @@ import webbrowser
 
 class MyApp(wx.App):
     gi = pygeoip.GeoIP('GeoLiteCity.dat')
+    rec = []
     
     def OnInit(self):
         self.res = xrc.XmlResource('GetIPLoc.xrc')
@@ -23,6 +23,7 @@ class MyApp(wx.App):
         
         # Bind Controls
         self.btnExit = xrc.XRCCTRL(self.frame, 'wxID_EXIT')
+        self.btnMap = xrc.XRCCTRL(self.frame, 'btnMap')
         self.listSummary = xrc.XRCCTRL(self.frame, 'listSummary')
         self.listSummary.InsertColumn(0, 'IP', width=110)
         self.listSummary.InsertColumn(1, 'Date')
@@ -35,6 +36,7 @@ class MyApp(wx.App):
         # Bind Events
         self.frame.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnClick, self.listSummary)
         self.frame.Bind(wx.EVT_BUTTON, self.OnClose, id=xrc.XRCID('wxID_EXIT'))
+        self.frame.Bind(wx.EVT_BUTTON, self.OnClickMap, id=xrc.XRCID('btnMap'))
         
         self.readLocFile()
         self.frame.Show()
@@ -61,20 +63,25 @@ class MyApp(wx.App):
         webbrowser.open_new(url)
             
     def OnClick(self, event):
-        rec = self.gi.record_by_name(event.GetText())
+        # ToDo, move this code out of here
+        self.rec = self.gi.record_by_name(event.GetText())
         
-        post_office = rec['postal_code']
+        post_office = self.rec['postal_code']
         if post_office == None:
             post_office = ''
         
-        self.txtArea.SetValue('%s' % (rec['area_code']))
+        self.txtArea.SetValue('%s' % (self.rec['area_code']))
         self.txtZip.SetValue('%s' % (post_office))
-        self.txtLatLon.SetValue('%s,%s' % (str(rec['latitude']), str(rec['longitude'])))
-        self.txtLoc.SetValue('%s, %s %s' % (rec['city'], rec['region_code'], rec['country_name']))
+        self.txtLatLon.SetValue('%s,%s' % (str(self.rec['latitude']), str(self.rec['longitude'])))
+        self.txtLoc.SetValue('%s, %s %s' % (self.rec['city'], self.rec['region_code'], self.rec['country_name']))
         
     def OnClose(self, evt):
         self.Exit()            
             
+    def OnClickMap(self, evt):
+        url = 'http://maps.google.com/?q=' + str(self.rec['latitude']) + ',' + str(self.rec['longitude'])
+        webbrowser.open_new(url)
+        
 if __name__ == '__main__':
     # The False argument says redirect stdout/stderr to a console instead of a window
     app = MyApp(False)
