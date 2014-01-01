f51ee015b6b022e2455061a4448038419ddf52b5
Tim King <timcking@gmail.com>
2013-12-31 19:16:31 -0800

    Work in progress

--- geoIP.py
+++ geoIP.py
@@ -28,8 +28,11 @@ class MyApp(wx.App):
         self.listSummary.InsertColumn(0, 'IP', width=110)
         self.listSummary.InsertColumn(1, 'Date')
         self.listSummary.InsertColumn(2, 'Time')        
+        self.txtLatLon = xrc.XRCCTRL(self.frame, 'txtLatLon')
+        self.txtLoc = xrc.XRCCTRL(self.frame, 'txtLoc')
         
         # Bind Events
+        self.frame.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnClick, self.listSummary)
         self.frame.Bind(wx.EVT_BUTTON, self.OnClose, id=xrc.XRCID('wxID_EXIT'))
         
         self.readLocFile()
@@ -49,30 +52,14 @@ class MyApp(wx.App):
         self.listSummary.SetStringItem(self.index, 1, theDate)
         self.listSummary.SetStringItem(self.index, 2, theTime)
         
-        rec = self.gi.record_by_name(tgt)
-        city = rec['city']
-        region = rec['metro_code']
-        country = rec['country_name']
-        lon = rec['longitude']
-        lat = rec['latitude']
+        # Don't need this
+        self.dictLocs[self.index] = tgt
         
-        # Save to dict here
-        #index_count = 0
-        #for theRec in rec:
-            #try:
-                #self.listSummary.Append("%s, %s %s" % (tgt, theDate, theTime))
-            #except Exception, e:
-                #print "Change me"
-            
-            # Save for when box is clicked
-            #self.dictTitles[index_count] = title.movieID
-            #index_count += 1         
-        
-        print 'IP Addr:   ' + tgt
-        print 'Date/Time: ' + theDate + ' ' + theTime
-        print 'Location:  ' + str(city)+', '+ str(region)+', '+ str(country)
-        print 'Lat/Lon:   ' + str(lat) + ','+ str(lon)
-        print ''
+        #print 'IP Addr:   ' + tgt
+        #print 'Date/Time: ' + theDate + ' ' + theTime
+        #print 'Location:  ' + str(city)+', '+ str(region)+', '+ str(country)
+        #print 'Lat/Lon:   ' + str(lat) + ','+ str(lon)
+        #print ''
         
         self.index += 1
     
@@ -81,6 +68,22 @@ class MyApp(wx.App):
         url = 'http://maps.google.com/?q=38.645,-121.4404'
         webbrowser.open_new(url)
             
+    def OnClick(self, event):
+        rec = self.gi.record_by_name(event.GetText())
+        city = rec['city']
+        region = rec['metro_code']
+        country = rec['country_name']
+        lon = rec['longitude']
+        lat = rec['latitude']
+        
+        post_office = rec['postal_code']
+        
+        if post_office == None:
+            post_office = ''
+        
+        self.txtLatLon.SetValue('%s, %s' % (str(rec['latitude']), str(rec['longitude'])))
+        self.txtLoc.SetValue('%s, %s %s %s' % (rec['city'], rec['region_code'], rec['country_name'], post_office))
+        
     def OnClose(self, evt):
         self.Exit()            
             
